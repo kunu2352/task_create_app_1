d2a5dcacce2c5ea15a33de98972dd5548ee1dc6d
EC2 Default User <ec2-user@ip-172-31-34-181.ap-northeast-1.compute.internal>
2023-10-16 23:46:15 +0000

    課題2[form作成]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,35 @@
 class BooksController < ApplicationController
   def new
+    @books = Book.new
+  end
+  def create
+    @books = Book.new(book_params)
+    if @books.save
+      flash[:notice] = "投稿に成功しました"
+      redirect_to book_path(@books.id)
+    else
+      flash.now[:aleart] = "投稿に失敗しました"
+      
+      render :new
+    end
   end
 
   def show
   end
 
   def index
+    
+    @bookers = Book.all
+    @books = Book.new
   end
+  
 
   def edit
   end
-end
+
+private
+  
+  def book_params
+  params.require(:book).permit(:title, :body)
+  end
+end  
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'tops/top'
-  resources :books
+  
+  resources :books, except: :new
+  get 'books' => 'books#new'
+  #resources :photos, except: :destroy
+  #resources :photos, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
